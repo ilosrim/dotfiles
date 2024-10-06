7b1e794769876fe1dec5190672763129f36ab3c8
ilosrim <ilosrim@yahoo.com>
2024-10-06 15:59:36 +0500

    add vscode-html-language-server

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,16 +4,17 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "tsserver", "phpactor", "intelephense", "volar", "tailwindcss" }
+local servers = { "html", "cssls", "vscode-html-language-server", "tsserver", "phpactor", "intelephense", "volar",
+    "tailwindcss" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+    lspconfig[lsp].setup {
+        on_attach = nvlsp.on_attach,
+        on_init = nvlsp.on_init,
+        capabilities = nvlsp.capabilities,
+    }
 end
 
 -- configuring single server, example: typescript
